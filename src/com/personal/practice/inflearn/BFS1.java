258db3286fac90a6eb1a92c980a4f92b6f0b04cc
powderBlue91 <dydrnjs518@gmail.com>
2021-10-28 16:08:32 +0900

    bfs,dfs 연습

---
+++ src/com/personal/practice/inflearn/BFS1.java
@@ -0,0 +1,45 @@
+package com.personal.practice.inflearn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node lt;
+    Node rt;
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
+public class BFS1 {
+    Node root;
+
+    public void BFS(Node node) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; ++i) {
+                Node cur = queue.poll();
+                System.out.println("cur" + cur.data);
+                if (cur.lt != null) queue.offer(cur.lt);
+                if (cur.rt != null) queue.offer(cur.rt);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        BFS1 main = new BFS1();
+        main.root = new Node(1);
+        main.root.lt = new Node(2);
+        main.root.rt = new Node(3);
+        main.root.lt.lt = new Node(4);
+        main.root.lt.rt = new Node(5);
+        main.root.rt.lt = new Node(6);
+        main.root.rt.rt = new Node(7);
+        main.BFS(main.root);
+    }
+}
---
+++ src/com/personal/practice/inflearn/DFS2.java
@@ -0,0 +1,33 @@
+package com.personal.practice.inflearn;
+
+// 부분집합 구하기(DFS)
+
+public class DFS2 {
+
+    static int num = 3;
+    static int [] arr;
+    public void DFS(int n) {
+        if (n == num+1) {
+            String tmp = "";
+            for (int i = 1; i <= num; ++i) {
+                if(arr[i] == 1) {
+                    tmp += (i + " ");
+                }
+            }
+            if (tmp.length() > 0)
+                System.out.println(tmp);
+            return;
+        }
+
+        arr[n] = 1;
+        DFS(n+1);
+        arr[n] = 0;
+        DFS(n+1);
+    }
+
+    public static void main(String[] args) {
+        DFS2 obj = new DFS2();
+        arr = new int[num+1];
+        obj.DFS(1);
+    }
+}
